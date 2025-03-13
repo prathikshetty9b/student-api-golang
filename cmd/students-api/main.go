9f5d183ceff5440e87da026c32bde534b1db077d
Prathik B Shetty <prathikshetty9b@gmail.com>
2025-03-13 21:18:54 +0530

    initial commit

---
+++ cmd/students-api/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Start the server
+	fmt.Println(" welcome to students api")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/prathikshetty9b/students-api
+
+go 1.24.1
